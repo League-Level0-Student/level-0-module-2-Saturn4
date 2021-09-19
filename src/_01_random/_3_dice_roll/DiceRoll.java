378e7ffde9765dd1e633957b7cd611e4a6feaf34
student <student@ip-172-31-60-148.us-west-2.compute.internal>
2021-09-19 11:40:00 -0700

    fix the dice

--- src/_01_random/_3_dice_roll/DiceRoll.java
+++ src/_01_random/_3_dice_roll/DiceRoll.java
@@ -48,20 +48,21 @@ public class DiceRoll {
 		rollButton.addActionListener((e) -> {
 
 			// 1. Make randomChoice equal to a random number between 1 and 6
-			int randomChoice = 0;
+			int randomChoice = 7;
+			// fix the button so it rolls the dice
 
 			// 2. Fix the code below so that it displays the correct image
-			if (randomChoice == 0) {
+			if (randomChoice == 1) {
 				label.setIcon(oneImg);
-			} else if (randomChoice == 0) {
+			} else if (randomChoice == 2) {
 				label.setIcon(twoImg);
-			} else if (randomChoice == 0) {
+			} else if (randomChoice == 3) {
 				label.setIcon(threeImg);
-			} else if (randomChoice == 0) {
+			} else if (randomChoice == 4) {
 				label.setIcon(fourImg);
-			} else if (randomChoice == 0) {
+			} else if (randomChoice == 5) {
 				label.setIcon(fiveImg);
-			} else {
+			} else if (randomChoice == 6) {
 				label.setIcon(sixImg);
 			}
 		});
@@ -73,6 +74,6 @@ public class DiceRoll {
 		window.setVisible(true);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.pack();
-	}
+	} //hello this is celeste
 
 }
