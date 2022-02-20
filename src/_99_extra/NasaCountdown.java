1eadbb75cec2b65330683fdedb06301b11cb5eae
student <student@ip-172-31-59-65.us-west-2.compute.internal>
2022-02-20 11:25:52 -0800

    help nasa countdown

--- src/_99_extra/NasaCountdown.java
+++ src/_99_extra/NasaCountdown.java
@@ -1,7 +1,9 @@
 
 package _99_extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown or the rocket won't launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -13,7 +15,7 @@ package _99_extra;
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
 		// 2. Use a pop-up to ask the user where to start counting from
-
+		JOptionPane.showInputDialog("Where do we star counting down from?");
 		// 3. Change the countdown to use the new starting point
 		
 		// 1. Print a countdown from 10 to 0 on the console
