b88e502374a13702569f7c76abe2e165b66371db
student <student@ip-172-31-58-102.us-west-2.compute.internal>
2022-02-27 11:26:41 -0800

    figure out why Star Show won't work

--- src/_99_extra/StarShow.java
+++ src/_99_extra/StarShow.java
@@ -14,48 +14,70 @@ public class StarShow {
 	
 	void makeStars() {
 		
-		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
+			//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
-
+		robot.setSpeed(8);
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+		int X = 10;
 		// 7. Make a variable to hold the Y position of the Robot and set it to 600
-
-		// 8. Make a variable to hold the star size and set it to 25
-
+		int Y = 600;
+		// 8. Make a variable to hold the star size and set it to 25 
+		int starSize = 25;
 		// 12. Repeat the steps #19 to #18, 30 times
-
+			int count = 0; 
+		while (count < 30) {
 			// 19. Set the pen width to i 
 	
 			// 10. Set the X position of the robot to your X variable
-	
+			robotLocationX(X);
 			// 11. Set the Y position of the robot to your Y variable
-	
+			robotLocationY(Y);
 			// 9. Call the drawStar() method with your star size variable
-	
+			drawStar(starSize);
 			// 14. Increase the X position by star size. See Figure 2.
-	
+			robotLocationX(25);
 			// 15. decrease the Y position by star size. See Figure 3.
-	
+			
 			// 16. Increase the star size by 20
 	
 			// 17. Turn the robot 12 degrees
 	
 			// 18. Make each star a different random color like in Figure 4. 
+			count++;
+		}
+	}
 
+	private int robotLocationY(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private int robotLocationX(int i) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+			robot.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
-
+			int count = 0; 
+			while (count < 5) {
+				robot.move(starSize);
+				robot.turn(144);
+				count++;
+			}
 			// 1. Move the robot the distance of the starSize variable
-	
+			
 			// 3. Turn the robot 144 degrees
 			
 	}
 	
+	private void count(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public static void main(String[] args) {
 		new StarShow().makeStars();
 	}
